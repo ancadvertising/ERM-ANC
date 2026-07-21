6c970f0de4a18ae7fe86338b8cbcba9a651fbec4
ancadvertising <anc.adv.agency@gmail.com>
2026-07-21 05:07:26 +0300

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 window.ANC_CONFIG = Object.freeze({
   APP_NAME: 'ANC ERP',
-  API_URL: 'PASTE_YOUR_GOOGLE_APPS_SCRIPT_EXEC_URL_HERE?api=v1',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxukmLS4-9rkt8Mgfe31GvG40NEUMZW9VMwBwDiWyYkDWCAQQ91DF9bLOtuAbk9ytpKSw/exec?api=v1',
   REQUEST_TIMEOUT_MS: 30000
 });
